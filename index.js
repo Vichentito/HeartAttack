d9c8598281fa6ee76c58946ec6ef393004e1e9db
Vichentito <chente54321@gmail.com>
2018-05-18 10:43:40 -0500

    aa

--- index.js
+++ index.js
@@ -7,15 +7,11 @@ var port = process.env.PORT || 9091;
 //mongodb://root:gatitosconsombrerodecopa@ds229435.mlab.com:29435/heartattack
 //'mongodb://localhost:27017/heartattack
 
-mongoose.connect('mongodb://root:gatitosconsombrerodecopa@ds229435.mlab.com:29435/heartattack', (err, res) => {
-	if(err){
-		throw err;
-	}else{
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://root:gatitosconsombrerodecopa@ds229435.mlab.com:29435/heartattack',{useMongoClient: true}).then(()=>{
 		console.log("La conexión a la base de datos está funcionando correctamente...");
 
 		app.listen(port, function(){
 			console.log("Servidor del api en http://localhost:"+port);
 		});
-		
-	}
-});
+	}).catch(err => console.log(err));
