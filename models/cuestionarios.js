0660984e564e1a3c706e271cd3777a3787b83dc1
Vicente Jafet Martinez Olivares <vicente.martinez@2bcore.com>
2018-05-18 07:11:14 -0500

    models

--- models/cuestionarios.js
+++ models/cuestionarios.js
@@ -6,7 +6,10 @@ var Schema = mongoose.Schema;
 var CuestionariosSchema = Schema({
 		numero: Numer,
 		preginta: String,
-		opciones: String,
+		opciones: {
+			op1:String,
+			op2:String,
+			op3:String},
 		respuesta: String
 });
 
